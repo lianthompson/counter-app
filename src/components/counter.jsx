18b66620759a86da8f171f2abe116fe4f1057c7f
Lian Thompson <lianythompson@gmail.com>
2019-06-14 14:58:35 -0700

    inline style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,18 +3,12 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
     };
-
-    styles = {
-        fontSize: 50,
-        fontWeight: 'bold'
-    }
 
     render() {
         return (
             <React.Fragment>
-                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
