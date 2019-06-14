0bb6b0a83a52fb3826a592b97bca73de096800b5
Lian Thompson <lianythompson@gmail.com>
2019-06-14 14:50:04 -0700

    add bootstrap

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,9 +9,8 @@ class Counter extends Component {
     render() {
         return (
             <React.Fragment>
-                <img src={this.state.imageUrl} alt="" />
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
     }
