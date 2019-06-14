e5b1657b3f67c2187ddbdc314a716015f8aeb230
Lian Thompson <lianythompson@gmail.com>
2019-06-14 14:51:52 -0700

    styles property

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,10 +6,15 @@ class Counter extends Component {
         imageUrl: 'https://picsum.photos/200'
     };
 
+    styles = {
+        fontSize: 50,
+        fontWeight: 'bold'
+    }
+
     render() {
         return (
             <React.Fragment>
-                <span className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         )
