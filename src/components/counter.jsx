05b83b90048240b78d27d4000950a5a0efa328d4
Lian Thompson <lianythompson@gmail.com>
2019-06-14 15:17:49 -0700

    dynamic styling and list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,15 +3,26 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     render() {
+
         return (
             <React.Fragment>
-                <span style={{ fontSize: 30 }} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
         )
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount() {
